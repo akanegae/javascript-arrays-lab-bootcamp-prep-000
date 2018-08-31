a9cf2e8f8ba7b411da27f7a44cb1570b8ab0a32f
Alison Kanegae <alisonkanegae@gmail.com>
2018-08-31 04:11:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,35 @@ function destructivelyPrependKitten(name) {
 }
 
 //4
-function destructivelyRemoveLastKitten(){
+function destructivelyRemoveLastKitten() {
   kittens.pop();
   return kittens;
+}
+
+//5
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return kittens;
+}
+
+//6
+function appendKitten(name) {
+  kittens = [...kittens, "Broom"];
+  return kittens;
+}
+
+//7
+function prependKitten(name) {
+  kittens = ["Arnold", ...kittens];
+  return kittens;
+}
+
+//8
+function removeLastKitten() {
+  return kittens.slice(2);
+}
+
+//9
+function removeFirstKitten() {
+  return kittens.slice(1);
 }
